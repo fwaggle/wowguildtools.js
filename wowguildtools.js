8c05b5e414ebd1b09d28b2ec0040ffbe7bff4934
Jamie Fraser <fwaggle@moodoo.org>
2013-06-30 11:23:54 +1000

    Clean up style of roster list, add ability to modify sorting and toggle thumbnails, AP, etc.

--- wowguildtools.js
+++ wowguildtools.js
@@ -65,6 +65,11 @@ function wowGuild(region, realm, guild) {
 	lnews = false;
 	lachievements = false;
 	lchallenges = false;
+	
+	lthumbnails = true;
+	lap = false;
+	
+	lsort = false;
 
 	this.news = function(value) {
 		lnews = value;
@@ -77,21 +82,38 @@ function wowGuild(region, realm, guild) {
 	this.challenges = function(value) {
 		lchallenges = value;
 	};
+
+	this.showThumbnails = function(value) {
+		lthumbnails = value;
+	};
+	
+	this.showAP = function(value) {
+		lap = value;
+	};
+
+	this.rosterSort = function(value) {
+		lsort = value;
+	};
 	
 	var roster_char_list = function(data) {
+		c = data.character;
 		d = '<li class="wow_char">';
-		d += '<img src="http://' + lregion + '.battle.net/static-render/';
-		d += lregion + '/' + data.character.thumbnail;
-		d += '?alt=/wow/static/images/2d/avatar/' + data.character.race;
-		d += '-' + data.character.gender + '.jpg" alt="';
-		d += data.character.name + '" />';
-		d += '<span class="name">' + data.character.name + '</span>';
-		d += '<span class="level">Level ' + data.character.level;
+		if (lthumbnails) {
+			d += '<img src="http://' + lregion + '.battle.net/static-render/';
+			d += lregion + '/' + c.thumbnail;
+			d += '?alt=/wow/static/images/2d/avatar/' + c.race;
+			d += '-' + c.gender + '.jpg" alt="';
+			d += c.name + '" />';
+		}
+		d += '<span class="name">' + c.name + '</span>';
+		d += '<span class="level">' + c.level;
 		d += '</span>';
-		d += '<span class="type">';
-		d += races[data.character.race] + ' ' + classes[data.character.class];
-		d += '</span>';
-		d += '<span class="ap">' + data.character.achievementPoints + '</span>';
+		d += '<span class="type"><span class="race">' + races[c.race];
+		d += '</span> <span class="class">' + classes[c.class];
+		d += '</span></span>';
+		if (lap) {
+			d += '<span class="ap">' + c.achievementPoints + '</span>';
+		}
 		d += '</li>';
 		return d;
 	};
@@ -99,9 +121,40 @@ function wowGuild(region, realm, guild) {
 	this.roster_render_list = function(layer) {
 		var chars = '<ul class="roster">';
 
-		roster_data.members.sort(function(a,b) {
-			return b.character.level - a.character.level;
-		});
+		switch (lsort) {
+			case 'level':
+				roster_data.members.sort(function(a,b) {
+					return b.character.level - a.character.level;
+				});
+				break;
+			case 'levelasc':
+				roster_data.members.sort(function(a,b) {
+					return a.character.level - b.character.level;
+				});
+				break;
+			case 'rank':
+				roster_data.members.sort(function(a,b) {
+					if (a.rank == b.rank) {
+						return b.character.level - a.character.level;
+					}
+					return a.rank - b.rank;
+				});
+				break;
+			case 'ap':
+				roster_data.members.sort(function(a,b) {
+					return b.character.achievementPoints - a.character.achievementPoints;
+				});
+				break;
+			case 'name':
+				roster_data.members.sort(function(a,b) {
+					x = a.character.name.toUpperCase();
+					y = b.character.name.toUpperCase();
+					return ((x < y) ? -1 : (x > y) ? +1 : 0);
+				});
+				break;
+			default:
+				break;
+		}
 
 		$.each(roster_data.members, function (i, da) {
 			chars += '<tr>';
